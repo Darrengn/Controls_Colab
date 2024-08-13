4d41e4585c5dd8e43f88275e1133ab1e9a33fc28
DarrenNg <ngwdarren@gmail.com>
2024-08-13 11:59:07 -0700

    changing kp values

--- PID_Colab/Diff_Drive_Sim4Colab.py
+++ PID_Colab/Diff_Drive_Sim4Colab.py
@@ -271,7 +271,7 @@ class PathSimulator():
                 err_ang = -err_ang
         
         v = 0.1*err_lin #linear velocity
-        w = 0.1*err_ang #steering
+        w = 1*err_ang #steering
         return v, w
 
 def main():
